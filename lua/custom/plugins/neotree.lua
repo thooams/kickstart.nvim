39a995e2bf487c793d46f6c961472bb1a2b825d0
Thomas HUMMEL <thomas@hummel.link>
2023-07-31 16:07:13 +0200

    Add spectre conf

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -208,7 +208,7 @@ return {
               --".null-ls_*",
             },
           },
-          follow_current_file = false, -- This will find and focus the file in the active buffer every
+          follow_current_file  = { enable = false }, -- This will find and focus the file in the active buffer every
                                        -- time the current file is changed while the tree is open.
           group_empty_dirs = false, -- when true, empty folders will be grouped together
           hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -235,7 +235,7 @@ return {
           }
         },
         buffers = {
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
+          follow_current_file = { enable = true }, -- This will find and focus the file in the active buffer every
                                        -- time the current file is changed while the tree is open.
           group_empty_dirs = true, -- when true, empty folders will be grouped together
           show_unloaded = true,
